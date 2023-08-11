390fed5ad3325040d81b1d94261c8f8f59e4ea65
Ahnaf Anan <aanana0171@gmail.com>
2023-08-11 18:32:15 +0600

    assignment

---
+++ src/AppNavbar.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+
+   <nav class="flex sm:justify-center space-x-4 font-medium text-base py-2"> 
+        <router-link class="nav-link text-gray-700" :to="{name:'home'}">Home</router-link>  
+        <router-link class="nav-link text-gray-700" :to="{name:'about'}">About</router-link> 
+        <router-link class="nav-link text-gray-700" :to="{name:'contact'}">Contact</router-link> 
+        <router-link class="nav-link text-gray-700" :to="{name:'service'}">Service</router-link>
+        <router-link class="nav-link text-gray-700" :to="{name:'login'}">Login</router-link>
+        <router-link class="nav-link text-gray-700" :to="{name:'register'}">Register</router-link>
+   </nav>
+  
+  <router-view/>
+</template>
+
+<style scoped></style>
